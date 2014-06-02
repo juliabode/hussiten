6461b13115174ce39d56d6a53aee55ad83ee142f
Julia Bode <julia.bode@hmmh.de>
2014-06-02 15:09:59 +0200

    Rename stylesheet.

--- lib/scripts.php
+++ lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9880649384aea9f1ee166331c0a30daa');
-  wp_enqueue_style('styles', get_template_directory_uri() . '/styles.css', false, '9880649384aea9f1ee166331c0a30daa');
+  wp_enqueue_style('styles', get_template_directory_uri() . '/style.css', false, '9880649384aea9f1ee166331c0a30daa');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
